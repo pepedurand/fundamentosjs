b56f393127ef2b924b5b166bc3e7c376eb146f39
Pepe <webpedrod@gmail.com>
2022-03-03 23:58:15 -0300

    Feat: subindo arquivos de infos do JS

---
+++ infos.js
@@ -0,0 +1,13 @@
+//tipagem dinâmica
+
+let minhaVar = 567;
+minhaVar = "texto";
+minhaVar = true;
+
+console.log (minhaVar) ;
+
+//multiparadigma - > várias formas de fazer a mesma coisa (flexível)
+
+//linguagem interpretada - diferente de compilada, significa que um programa lê o que será escrito no código
+
+//nodeJS - Não é uma linguagem, é um interpretador de JS
