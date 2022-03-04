d0ee0df52434351751fab3f1bacd5759a4226960
Pepe <webpedrod@gmail.com>
2022-03-03 23:28:26 -0300

    Feat: subindo arquivos de exemplos de conversao JS

---
+++ conversoes.js
@@ -0,0 +1,14 @@
+//tipo de dado
+//booleanos
+
+//conversao implicita
+const numero = 3;
+const numeroString = "4a";
+
+
+//Number()
+//String()
+console.log (numero + Number(numeroString));
+
+
+//conversao explicita
