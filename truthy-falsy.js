a61823bd0c6968d6ab83a40f1953daeee140cfff
Pepe <webpedrod@gmail.com>
2022-03-03 22:44:09 -0300

    Feat: tipos de variaveis e truthy, falsy

---
+++ truthy-falsy.js
@@ -0,0 +1,32 @@
+const usuarioLogado = true;
+const contapaga = false;
+
+//truthy ou falsy
+
+// 0 => false
+// 1 => true 
+
+console.log (0 == false);
+console.log (''== false);
+console.log (1 == true);
+
+//null
+//undefined
+
+console.log (null == false);
+console.log (undefined == false);
+console.log (null == true);
+console.log (undefined == true);
+
+let minhaVar;
+let varNull = null;
+console.log (minhaVar)
+console.log (varNull)
+
+let numero=3;
+let texto='alura';
+
+console.log (typeof numero);
+console.log (typeof texto);
+console.log (typeof minhaVar);
+console.log (typeof varNull);
--- type-var.js
+++ type-var.js
@@ -7,9 +7,35 @@
 //
 //var area;
 
-let altura = 5;
-let comprimento = 7;
+//let altura = 5;
+//let comprimento = 7;
+//
+//area = altura * comprimento;
+//console.log(area)
 
-area = altura * comprimento;
-console.log(area)
+//et forma = 'retângulo';
+//et altura = 5;
+//et comprimento = 7;
+//et area;
+//
+//f (forma === 'retângulo') {
+//   area = altura * comprimento;
+// else {
+//   area = altura * comprimento / 2;
+//
+//
+//onsole.log (area);
+
+const forma = 'triangulo';
+const altura = 5;
+const largura = 6;
+let area;
+
+if (forma === 'quadrado') {
+    area = altura *largura;
+} else {
+    console.log('não é um quadrado');
+}
+
+console.log (area);
 
